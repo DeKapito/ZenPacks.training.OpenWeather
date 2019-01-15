0c0de708b50d5c61f20f8154335d11cda8643f29
DeKapito <kapitula_ak16@nuwm.edu.ua>
2019-01-15 17:52:48 +0200

    SVC-xxxx: initial zpdir setup

---
+++ ZenPacks/training/OpenWeather/dsplugins.py
@@ -0,0 +1,96 @@
+"""Monitors current conditions using the Weather Underground API."""
+
+# Logging
+import logging
+LOG = logging.getLogger('zen.OpenWeather')
+
+# stdlib Imports
+import json
+import time
+
+from Products.DataCollector.plugins.DataMaps import ObjectMap
+
+# Twisted Imports
+from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.web.client import getPage
+
+# PythonCollector Imports
+from ZenPacks.zenoss.PythonCollector.datasources.PythonDataSource import (
+    PythonDataSourcePlugin,
+    )
+
+
+class Conditions(PythonDataSourcePlugin):
+
+    """Open Weather conditions data source plugin."""
+
+    @classmethod
+    def config_key(cls, datasource, context):
+        return (
+            context.device().id,
+            datasource.getCycleTime(context),
+            context.id,
+            'oweather-conditions',
+            )
+
+    @classmethod
+    def params(cls, datasource, context):
+        return {
+            'api_key': context.zOWeatherAPIKey,
+            'city_name': context.city_name,
+            'country_code': context.country_code,
+            'location_name': context.title,
+            }
+
+    @inlineCallbacks
+    def collect(self, config):
+        data = self.new_data()
+
+        for datasource in config.datasources:
+            try:
+                response = yield getPage(
+                    'https://api.openweathermap.org/data/2.5/weather?q={city_name},{country_code}&appid={api_key}'
+                    .format(
+                        api_key=datasource.params['api_key'],
+                        city_name=datasource.params['city_name'],
+                        country_code=datasource.params['country_code']))
+
+                response = json.loads(response)
+            except Exception:
+                LOG.exception(
+                    "%s: failed to get conditions data for %s",
+                    config.id,
+                    datasource.params['location_name'])
+
+                continue
+
+            weather = response['weather'][0]
+            main = response['main']
+            wind = response['wind']
+            for datapoint_id in (x.id for x in datasource.points):
+                for block in (weather, main, wind):
+                    if datapoint_id not in block:
+                        continue
+
+                    try:
+                        value = block[datapoint_id]
+                        if isinstance(value, basestring):
+                            value = value.strip(' %')
+
+                        value = float(value)
+                    except (TypeError, ValueError):
+                        # Sometimes values are NA or not available.
+                        continue
+
+                    dpname = '_'.join((datasource.datasource, datapoint_id))
+                    data['values'][datasource.component][dpname] = (value, 'N')
+
+            data['maps'].append(
+                ObjectMap({
+                    'relname': 'oweatherLocations',
+                    'modname': 'ZenPacks.training.OpenWeather.OWeatherLocation',
+                    'id': datasource.component,
+                    'weather': response['weather'][0]['description']
+                }))
+
+        returnValue(data)
---
+++ ZenPacks/training/OpenWeather/modeler/plugins/OpenWeather/Locations.py
@@ -0,0 +1,81 @@
+"""Models locations using the Open Weather API."""
+
+# stdlib Imports
+import json
+import urllib
+
+# Twisted Imports
+from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.web.client import getPage
+
+# Zenoss Imports
+from Products.DataCollector.plugins.CollectorPlugin import PythonPlugin
+
+
+class Locations(PythonPlugin):
+
+    """Open Weather locations modeler plugin."""
+
+    relname = 'oweatherLocations'
+    modname = 'ZenPacks.training.OpenWeather.OWeatherLocation'
+
+    requiredProperties = (
+        'zOWeatherAPIKey',
+        'zOWeatherLocations',
+        )
+
+    deviceProperties = PythonPlugin.deviceProperties + requiredProperties
+
+    @inlineCallbacks
+    def collect(self, device, log):
+        """Asynchronously collect data from device. Return a deferred."""
+        log.info("%s: collecting data", device.id)
+
+        apikey = getattr(device, 'zOWeatherAPIKey', None)
+        if not apikey:
+            log.error(
+                "%s: %s not set. Get one from https://openweathermap.org/api",
+                device.id,
+                'zOWeatherAPIKey')
+
+            returnValue(None)
+
+        locations = getattr(device, 'zOWeatherLocations', None)
+        if not locations:
+            log.error(
+                "%s: %s not set.",
+                device.id,
+                'zOWeatherLocations')
+
+            returnValue(None)
+
+        rm = self.relMap()
+
+        for location in locations:
+            try:
+                response = yield getPage(
+                    'http://autocomplete.wunderground.com/aq?query={query}'
+                    .format(query=urllib.quote(location)))
+
+                response = json.loads(response)
+            except Exception, e:
+                log.error(
+                    "%s: %s", device.id, e)
+
+                returnValue(None)
+
+            for result in response['RESULTS']:
+                rm.append(self.objectMap({
+                    'id': self.prepId(result['zmw']),
+                    'title': result['name'],
+                    'api_link': result['l'],
+                    'city_name': result['name'].split(',')[0],
+                    'country_code': result['c'],
+                    'timezone': result['tzs'],
+                    }))
+
+        returnValue(rm)
+
+    def process(self, device, results, log):
+        """Process results. Return iterable of datamaps or None."""
+        return results
